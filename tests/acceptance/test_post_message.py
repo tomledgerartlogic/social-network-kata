0034217ef523f11c05b0669dd223ec00b03d5114
mheyw <50662869+mheyw@users.noreply.github.com>
2024-11-29 15:03:40 +0000

    Tom Acceptance Test WIP

--- tests/acceptance/test_post_message.py
+++ tests/acceptance/test_post_message.py
@@ -1,3 +1,18 @@
+import MessagePrinter
+import MessageClock
+
 class TestPostMessage:
     def test_user_can_post_message_to_timeline(self):
-        assert True
+        # Arrange
+        clock = Mock(MessageClock)
+        printer = Mock(MessagePrinter)
+
+        test_message = "I love the weather today (5 minutes ago)"
+
+        # Act
+        # > Alice -> I love the weather today 
+        # > Alice
+
+        # Assert
+        # Test message equals message passed in to printer
+        printer.print.assert_called_once_with(test_message)
