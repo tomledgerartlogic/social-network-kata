218a765792b8a58cb71bf3e08c84994729928506
mheyw <50662869+mheyw@users.noreply.github.com>
2024-11-29 15:32:09 +0000

    David Acceptance Test Done?

--- tests/acceptance/test_post_message.py
+++ tests/acceptance/test_post_message.py
@@ -1,18 +1,29 @@
 import MessagePrinter
 import MessageClock
+import SocialMediaService
+import SocialMedia
+from unittest import patch
 
 class TestPostMessage:
+    @patch('builtins.input', side_effect=['Alice -> I love the weather today', 'Alice', 'exit'])
     def test_user_can_post_message_to_timeline(self):
         # Arrange
         clock = Mock(MessageClock)
         printer = Mock(MessagePrinter)
 
-        test_message = "I love the weather today (5 minutes ago)"
+        expected_output = "I love the weather today (5 minutes ago)"
 
+        social_media_service = SocialMediaService(clock, printer)
+        ##accept command
+        social_media = SocialMedia(StandardInputter)
+                
         # Act
         # > Alice -> I love the weather today 
         # > Alice
+        # > exit
+
+        social_media.run()
 
         # Assert
         # Test message equals message passed in to printer
-        printer.print.assert_called_once_with(test_message)
+        printer.print.assert_called_once_with(expected_output)
